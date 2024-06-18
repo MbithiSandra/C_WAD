dcc4d2a69412521bd0ee97e2b38e70f62b2579c4
Sandra <sandra.mbithi@strathmore.edu>
2024-06-18 19:27:22 +0300

    creation of a new footer CSS file

---
+++ Template/footer.php
@@ -0,0 +1,35 @@
+<section class="footer">
+    <div class="box-container">
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="Home.html"> <i class="fas fa-angle-right"></i>HOME</a>
+            <a href="About.html"> <i class="fas fa-angle-right"></i>ABOUT</a>
+            <a href="Projects.html"> <i class="fas fa-angle-right"></i>PROJECTS</a>
+            <a href="Book.html"> <i class="fas fa-angle-right"></i>BOOK</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="#"> <i class="fas fa-angle-right"></i>Ask Questions</a>
+            <a href="#"> <i class="fas fa-angle-right"></i>About Us</a>
+            <a href="#"> <i class="fas fa-angle-right"></i>Privacy Policy</a>
+            <a href="#"> <i class="fas fa-angle-right"></i>Terms of Use</a>
+        </div>
+
+        <div class="box">
+            <h3>Contact Info</h3>
+            <a href="#"> <i class="fas fa-phone"></i>+254712307083</a>
+            <a href="#"> <i class="fas fa-phone"></i>+254720416466</a>
+            <a href="#"> <i class="fas fa-envelope"></i>alemserviceslimited@gmail.com</a>
+            <a href="#"> <i class="fas fa-map"></i>Nairobi, Kenya</a>
+        </div>
+        <div class="box">
+            <h3>Follow us</h3>
+            <a href="#"> <i class="fab fa-facebook"></i>Facebook</a>
+            <a href="#"> <i class="fab fa-twitter"></i> Twitter</a>
+            <a href="#"> <i class="fab fa-instagram"></i>Instagram</a>
+        </div>
+    </div>
+    <div class="credit">Created by <span>Sandra Minoo</span> | All rights reserved! </div>
+
+</section>
