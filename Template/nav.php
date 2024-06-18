640bbc8046a298edd5e992dcfbd9d31a937f08e5
Sandra <sandra.mbithi@strathmore.edu>
2024-06-18 17:56:37 +0300

    changing the navigation

--- Template/nav.php
+++ Template/nav.php
@@ -8,12 +8,12 @@
                 
                   <li><a href="index.php">HOME</a></li>
                   <li><a href="about.php">ABOUT</a></li>
-                  <li><a href="animals.html">OUR ANIMALS</a></li>
-                  <li><a href="products.html">PRODUCTS</a></li>
-                  <li><a href="farmtour.html">FARMTOURS</a></li>
-                  <li><a href="contactus.html">CONTACT</a></li>
-                  <li><a href="blog.html">BLOG/NEWS</a></li>
-                  <li><a href="login.html" >LOGIN</a></li>
+                  <li><a href="animals.php">OUR ANIMALS</a></li>
+                  <li><a href="products.php">PRODUCTS</a></li>
+                  <li><a href="farmtour.php">FARMTOURS</a></li>
+                  <li><a href="contactus.php">CONTACT</a></li>
+                  <li><a href="blog.php">BLOG/NEWS</a></li>
+                  <li><a href="login.php" >LOGIN</a></li>
               </ul>
           </nav>
       </section>
