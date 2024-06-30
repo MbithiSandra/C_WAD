012cc421a3927a286f69e31b0c7ed32d930724d7
Sandra <sandra.mbithi@strathmore.edu>
2024-06-30 13:04:52 +0300

    inserting default content into employee table

--- Database/thefarm.sql
+++ Database/thefarm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2024 at 11:48 AM
+-- Generation Time: Jun 30, 2024 at 12:03 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,19 +30,29 @@ USE `thefarm`;
 --
 
 DROP TABLE IF EXISTS `employee`;
-CREATE TABLE `employee` (
-  `EmployeeID` bigint(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `employee` (
+  `EmployeeID` bigint(10) NOT NULL AUTO_INCREMENT,
   `Fullname` varchar(100) NOT NULL,
   `DateOfBirth` date NOT NULL,
   `JobTitle` varchar(50) NOT NULL,
-  `EmploymentStatus` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `EmploymentStatus` varchar(50) NOT NULL,
+  PRIMARY KEY (`EmployeeID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Truncate table before insert `employee`
 --
 
 TRUNCATE TABLE `employee`;
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`EmployeeID`, `Fullname`, `DateOfBirth`, `JobTitle`, `EmploymentStatus`) VALUES
+(1, 'Sandra Mbithi', '1994-06-02', 'Farm Manager', 'Full time'),
+(2, 'Mariposa Victor', '1984-05-04', 'Veterinarian', 'Full Time'),
+(3, 'Peter Kimani', '1997-09-13', 'Farm Hand', 'Part Time');
+
 -- --------------------------------------------------------
 
 --
@@ -50,12 +60,13 @@ TRUNCATE TABLE `employee`;
 --
 
 DROP TABLE IF EXISTS `trainer`;
-CREATE TABLE `trainer` (
+CREATE TABLE IF NOT EXISTS `trainer` (
   `TrainerID` varchar(11) NOT NULL,
   `TrainerName` varchar(50) NOT NULL,
   `Specialization` varchar(50) NOT NULL,
   `YearsOfExperience` int(50) NOT NULL,
-  `EmploymentStatus` varchar(50) NOT NULL
+  `EmploymentStatus` varchar(50) NOT NULL,
+  PRIMARY KEY (`TrainerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -70,59 +81,22 @@ TRUNCATE TABLE `trainer`;
 --
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `UserID` bigint(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `UserID` bigint(11) NOT NULL AUTO_INCREMENT,
   `Fullname` varchar(50) DEFAULT NULL,
   `Email` varchar(50) NOT NULL,
   `Username` varchar(50) NOT NULL,
-  `Password` varchar(50) DEFAULT NULL
+  `Password` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`UserID`),
+  UNIQUE KEY `Email` (`Email`),
+  UNIQUE KEY `Username` (`Username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Truncate table before insert `users`
 --
 
-TRUNCATE TABLE `users`;
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `employee`
---
-ALTER TABLE `employee`
-  ADD PRIMARY KEY (`EmployeeID`);
-
---
--- Indexes for table `trainer`
---
-ALTER TABLE `trainer`
-  ADD PRIMARY KEY (`TrainerID`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`UserID`),
-  ADD UNIQUE KEY `Email` (`Email`),
-  ADD UNIQUE KEY `Username` (`Username`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `employee`
---
-ALTER TABLE `employee`
-  MODIFY `EmployeeID` bigint(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `UserID` bigint(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
+TRUNCATE TABLE `users`;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
